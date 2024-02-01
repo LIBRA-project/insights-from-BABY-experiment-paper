451b9274ab65b47c17dd23b41c805184f92cd462
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-02-01 10:31:31 -0500

    documentation

--- neutron_detection/diamond/process_data.py
+++ neutron_detection/diamond/process_data.py
@@ -2,7 +2,19 @@ import os
 import numpy as np
 
 
-def get_time_energy_values(directory):
+def get_time_energy_values(
+    directory: str, time_column: int = 2, energy_column: int = 3
+):
+    """From a directory of CSV files, extract the time and energy values.
+
+    Args:
+        directory (str): The directory containing the CSV files.
+        time_column (int, optional): The column containing the time values. Defaults to 2.
+        energy_column (int, optional): The column containing the energy values. Defaults to 3.
+
+    Returns:
+        np.ndarray, np.ndarray: A tuple containing all the time and energy values.
+    """
     energy_values = []
     time_values = []
 
@@ -14,17 +26,24 @@ def get_time_energy_values(directory):
             data = np.genfromtxt(csv_file_path, delimiter=";")
             # print(data.shape)
             # print(data)
-            # Assuming the energy values are in a specific column (e.g., column 1)
-            time_column = 2
-            energy_column = 3
-
             # Extract and append the energy data to the list
             time_values.extend(data[:, time_column])
             energy_values.extend(data[:, energy_column])
     return time_values, energy_values
 
 
-def main(directory, bin_time, energy_peak_min, energy_peak_max):
+def main(directory: str, bin_time: int, energy_peak_min: float, energy_peak_max: float):
+    """
+
+    Args:
+        directory (str): The directory containing the CSV files.
+        bin_time (int): The time interval to bin the data (s).
+        energy_peak_min (float): The minimum energy channel for the (n,alpha) peak.
+        energy_peak_max (float): The maximum energy channel for the (n,alpha) peak.
+
+    Returns:
+        dict: A dictionary containing the time and energy values, as well as the count rates.
+    """
     time_values, energy_values = get_time_energy_values(directory)
 
     # Create a histogram to represent the combined energy spectrum
@@ -62,7 +81,19 @@ def main(directory, bin_time, energy_peak_min, energy_peak_max):
     return res
 
 
-def get_avg_neutron_rate(time_values, peak_time_values, window):
+def get_avg_neutron_rate(
+    time_values: np.ndarray, peak_time_values: np.ndarray, window: list
+):
+    """Calculate the average neutron rate for a given time window.
+
+    Args:
+        time_values (np.ndarray): The time values.
+        peak_time_values (np.ndarray): The time values for the (n,alpha) peak.
+        window (list): The time window for the average neutron rate.
+
+    Returns:
+        dict: A dictionary containing the average neutron rate for the time window.
+    """
     section_data = {}
     # Create mask to only count pulses of any energy in section time window
 
